d3f2dda9c483219a8cbabb235f3dd61d499f06e0
o-ibiloye <o.ibiloye@fangocloud.co.uk>
2023-02-06 15:10:59 +0000

    Tutorial 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from "react";
 import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -14,7 +15,7 @@ const AboutPage = () => {
 };
 
 // Step 3: Add Title for the about page
-export const Head = () => <title>About Me</title>;
+export const Head = () => <Seo title="About Me" />;
 
 // Step 4: Export your component
 export default AboutPage;
